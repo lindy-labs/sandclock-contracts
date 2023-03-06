5587b0d2868a93bed3ca7fe5ebf402dc518a1aca
brkomir <91959886+brkomir@users.noreply.github.com>
2023-03-06 17:17:25 +0100

    Update src/steth/scUSDC.sol
    Co-authored-by: coolhill <8256597+coolhill@users.noreply.github.com>

--- src/steth/scUSDC.sol
+++ src/steth/scUSDC.sol
@@ -64,7 +64,7 @@ contract scUSDC is sc4626 {
     // USDC / WETH target LTV
     uint256 public targetLtv = 0.65e18;
     // max slippage for swapping WETH -> USDC
-    uint256 public slippageTolerance = 0.01e18; // 1% default
+    uint256 public slippageTolerance = 0.99e18; // 1% default
 
     // leveraged (w)eth vault
     ERC4626 public immutable scWETH;
