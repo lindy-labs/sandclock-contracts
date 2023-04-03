d398ee7b010d1e6b4df1c813b340a916de5dbc6a
coolhill <8256597+coolhill@users.noreply.github.com>
2023-04-03 13:53:00 +0200

    Update src/steth/scWETH.sol

--- src/steth/scWETH.sol
+++ src/steth/scWETH.sol
@@ -361,7 +361,7 @@ contract scWETH is sc4626, IFlashLoanRecipient {
         totalInvested += amount;
 
         // when deleveraging, withdraw extra to cover slippage
-        if (!isDeposit) amount = flashLoanAmount.mulWadDown(C.ONE - slippageTolerance);
+        if (!isDeposit) amount += flashLoanAmount.mulWadDown(C.ONE - slippageTolerance);
 
         // take flashloan
         balancerVault.flashLoan(address(this), tokens, amounts, abi.encode(isDeposit, amount));
