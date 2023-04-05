ff542760f067d56801b74c063a6a6a6b9007e62d
brkomir <nenad.milenkovic154@outlook.com>
2023-04-05 23:20:52 +0200

    compound v2 interfaces & usage example

---
+++ src/interfaces/compound/CErc20.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface CErc20 {
+    /**
+     * @notice Sender supplies assets into the market and receives cTokens in exchange
+     * @dev Accrues interest whether or not the operation succeeds, unless reverted
+     * @param mintAmount The amount of the underlying asset to supply
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function mint(uint256 mintAmount) external returns (uint256);
+
+    /**
+     * @notice Sender borrows assets from the protocol to their own address
+     * @param borrowAmount The amount of the underlying asset to borrow
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function borrow(uint256 borrowAmount) external returns (uint256);
+
+    /**
+     * @notice Sender repays their own borrow
+     * @param repayAmount The amount to repay
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function repayBorrow(uint256 repayAmount) external returns (uint256);
+
+    /**
+     * @notice Get the token balance of the `owner`
+     * @param owner The address of the account to query
+     * @return The number of tokens owned by `owner`
+     */
+    function balanceOf(address owner) external view returns (uint256);
+
+    /**
+     * @notice Get the underlying balance of the `owner`
+     * @dev This also accrues interest in a transaction
+     * @param owner The address of the account to query
+     * @return The amount of underlying owned by `owner`
+     */
+    function balanceOfUnderlying(address owner) external returns (uint256);
+
+    /**
+     * @notice Accrue interest to updated borrowIndex and then calculate account's borrow balance using the updated borrowIndex
+     * @param account The address whose balance should be calculated after updating borrowIndex
+     * @return The calculated balance
+     */
+    function borrowBalanceCurrent(address account) external returns (uint256);
+
+    /**
+     * @notice Returns the current per-block borrow interest rate for this cToken
+     * @return The borrow interest rate per block, scaled by 1e18
+     */
+    function borrowRatePerBlock() external view returns (uint256);
+
+    /**
+     * @notice Returns the current per-block supply interest rate for this cToken
+     * @return The supply interest rate per block, scaled by 1e18
+     */
+    function supplyRatePerBlock() external view returns (uint256);
+}
---
+++ test/CompoundExample.t.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "forge-std/Test.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+
+import {Constants as C} from "../src/lib/Constants.sol";
+import {CErc20} from "../src/interfaces/compound/CErc20.sol";
+import {CEther} from "../src/interfaces/compound/CEther.sol";
+import {Comptroller} from "../src/interfaces/compound/Comptroller.sol";
+
+contract CompoundExample is Test {
+    using FixedPointMathLib for uint256;
+
+    function test_compound() public {
+        uint256 forkId = vm.createFork(vm.envString("RPC_URL_MAINNET"));
+        vm.selectFork(forkId);
+        vm.rollFork(16643381);
+        ERC20 usdc = ERC20(C.USDC);
+
+        uint256 deposit = 10_000e6;
+        deal(address(usdc), address(this), deposit);
+
+        CErc20 cUSDC = CErc20(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
+        usdc.approve(address(cUSDC), type(uint256).max);
+        uint256 mintResult = cUSDC.mint(deposit); // 0 = success
+        console2.log("mintResult", mintResult);
+        assertEq(mintResult, 0, "mintResult");
+
+        Comptroller comptroller = Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
+
+        CEther cETH = CEther(0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5);
+        address[] memory cTokens = new address[](2);
+        cTokens[0] = address(cETH);
+        cTokens[1] = address(cUSDC);
+        uint256[] memory enterResults = comptroller.enterMarkets(cTokens);
+        console2.log("enterResults", enterResults.length);
+        assertEq(enterResults.length, 2, "no result for entering the market");
+        console2.log("enterResults[0]", enterResults[0]);
+        console2.log("enterResults[1]", enterResults[1]);
+        assertEq(enterResults[0], 0, "did not enter the cETH market");
+        assertEq(enterResults[1], 0, "did not enter the cUSDC market");
+
+        uint256 borrowAmount = 1e18;
+        uint256 balanceBefore = address(this).balance;
+        uint256 borrowResult = cETH.borrow(borrowAmount);
+        console2.log("borrowResult", borrowResult);
+        assertEq(borrowResult, 0, "borrow failed");
+        console2.log("borrowed eth", address(this).balance - balanceBefore);
+        assertEq(cETH.borrowBalanceCurrent(address(this)), borrowAmount, "borrow amount not correct");
+
+        cETH.repayBorrow{value: borrowAmount}();
+        console2.log("balance after debt is repayed", address(this).balance - balanceBefore);
+
+        uint256 usdcCollateralFactor = comptroller.markets(address(cUSDC)).collateralFactorMantissa;
+        console2.log("usdcCollateralFactor", usdcCollateralFactor); // 855000000000000000
+
+        uint256 usdcSupplyRatePerBlock = cUSDC.supplyRatePerBlock();
+        uint256 blocksPerYear = 2102400; // estimated as 2102400 assuming 13.2 seconds per block
+        uint256 usdcSupplyAPR = usdcSupplyRatePerBlock * blocksPerYear;
+        console2.log("usdcSupplyAPR", usdcSupplyAPR); // 0.015080590556323200 ~ 1.5%
+    }
+
+    receive() external payable {
+        console2.log("received", msg.value);
+    }
+}
