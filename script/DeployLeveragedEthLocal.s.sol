fa11463532c9e87a2ee568efb046b1ef64044e69
0xrin1 <0xrin1@protonmail.com>
2023-04-19 17:54:28 +0200

    harvest

--- script/DeployLeveragedEthLocal.s.sol
+++ script/DeployLeveragedEthLocal.s.sol
@@ -21,23 +21,27 @@ contract DeployScript is DeployLeveragedEth, Test {
     }
 
     function fixtures() internal {
-        console2.log("executing steth fixtures");
+        console2.log("\nexecuting steth fixtures\n");
 
         fund();
 
-        uint256 depositAmount = 10e18;
-        deposit(alice, address(wethContract), depositAmount);
+        deposit(alice, address(wethContract), 10e18);
+
+        harvest();
     }
 
     function fund() internal {
         console2.log("funding");
         deal(alice, 10e18);
         deal(keeper, 10e18);
+        deal(address(weth), 100e18);
         deal(address(weth), alice, 100e18);
+        deal(address(weth), keeper, 100e18);
     }
 
     function deposit(address from, address to, uint256 amount) internal {
         console2.log("depositing");
+
         vm.startPrank(from);
         weth.approve(address(to), type(uint256).max);
         wethContract.deposit(amount, from);
@@ -45,8 +49,10 @@ contract DeployScript is DeployLeveragedEth, Test {
     }
 
     function harvest() internal {
-        console2.log("harvesting");
+        console2.log("harvesting", keeper);
+
         vm.prank(keeper);
         wethContract.harvest();
+        vm.stopPrank();
     }
 }
