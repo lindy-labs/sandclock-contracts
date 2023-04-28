c637dada5d6704a4dcbd9c46e18430be9b24a00d
0xrin1 <0xrin1@protonmail.com>
2023-04-28 02:32:30 +0200

    simulate profit

--- script/DeployLeveragedEthLocal.s.sol
+++ script/DeployLeveragedEthLocal.s.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.13;
 import "forge-std/console2.sol";
 import "forge-std/Test.sol";
 
-import {DeployLeveragedEth} from "./base/DeployLeveragedEth.sol";
-import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
-import {MockWETH} from "../test/mocks/MockWETH.sol";
-import {MockUSDC} from "../test/mocks/MockUSDC.sol";
-import {sc4626} from "../src/sc4626.sol";
-import {scWETH} from "../src/steth/scWETH.sol";
-import {scUSDC} from "../src/steth/scUSDC.sol";
+import { DeployLeveragedEth } from "./base/DeployLeveragedEth.sol";
+import { FixedPointMathLib } from "solmate/utils/FixedPointMathLib.sol";
+import { MockWETH } from "../test/mocks/MockWETH.sol";
+import { MockUSDC } from "../test/mocks/MockUSDC.sol";
+import { sc4626 } from "../src/sc4626.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { scWETH } from "../src/steth/scWETH.sol";
+import { scUSDC } from "../src/steth/scUSDC.sol";
+import { Constants as C } from "../src/lib/Constants.sol";
 
 contract DeployScript is DeployLeveragedEth, Test {
     using FixedPointMathLib for uint256;
@@ -41,18 +43,21 @@ contract DeployScript is DeployLeveragedEth, Test {
 
         fund();
 
-        deposit(wethContract);
-        // deposit(usdcContract);
+        depositForUsers(weth, wethContract);
+        depositForUsers(usdc, usdcContract);
 
         rebalance(wethContract);
-        // rebalance(usdcContract);
+        rebalance(usdcContract);
+
+        profit();
 
         redeem(alice);
     }
 
-    function deposit(sc4626 vaultToken) internal {
-        deposit(alice, address(vaultToken), 10e18);
-        deposit(bob, address(vaultToken), 10e18);
+    function depositForUsers(ERC20 asset, sc4626 vaultToken) internal {
+        console2.log("depositing for users", 100 * 10 ** asset.decimals());
+        deposit(asset, vaultToken, alice, address(vaultToken), 10 * 10 ** asset.decimals());
+        deposit(asset, vaultToken, bob, address(vaultToken), 10 * 10 ** asset.decimals());
     }
 
     function fund() internal {
@@ -65,33 +70,63 @@ contract DeployScript is DeployLeveragedEth, Test {
         deal(address(curveEthStEthPool), 100e18);
 
         // Dole out WETH
-        deal(address(weth), 100e18);
+        deal(address(weth), 200e18);
         deal(address(weth), alice, 100e18);
         deal(address(weth), bob, 100e18);
+
+        // Dole out USDC
+        deal(address(usdc), alice, 100e6);
+        deal(address(usdc), bob, 100e6);
     }
 
-    function deposit(address from, address to, uint256 amount) internal {
+    function deposit(
+        ERC20 asset,
+        sc4626 vault,
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
         console2.log("depositing", from);
 
         vm.startPrank(from);
-        weth.approve(address(to), type(uint256).max);
-        wethContract.deposit(amount, from);
+        asset.approve(address(to), type(uint256).max);
+        vault.deposit(amount, from);
         vm.stopPrank();
     }
 
     function rebalance(scWETH vaultToken) internal {
-        console2.log("rebalancing");
+        console2.log("rebalancing scWETH");
 
-        vm.prank(keeper);
+        vm.startPrank(keeper);
         vaultToken.harvest();
         vm.stopPrank();
     }
 
     function rebalance(scUSDC vaultToken) internal {
-        console2.log("rebalancing");
+        console2.log("rebalancing scUSDC");
 
-        vm.prank(keeper);
+        vm.startPrank(keeper);
         vaultToken.rebalance();
+        vm.stopPrank();
+    }
+
+    function profit() internal {
+        console2.log("generate profit for scUSDC vault");
+
+        (, uint256 borrowAmount) = aavePool.book(
+            address(wethContract),
+            address(weth)
+        );
+
+        // Reduce amount of debt so that there is less debt than the original to create profit
+        vm.startPrank(address(wethContract));
+        deal(address(weth), address(wethContract), 100e18);
+        aavePool.repay(
+            address(weth),
+            borrowAmount,
+            C.AAVE_VAR_INTEREST_RATE_MODE, // does not impact mock
+            address(wethContract) // does not impact mock
+        );
         vm.stopPrank();
     }
 
