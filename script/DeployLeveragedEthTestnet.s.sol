31c4e9701e5a509cf4fbae8ce423fb9847049517
0xrin1 <0xrin1@protonmail.com>
2023-05-03 19:06:34 +0200

    GoerliConstants

--- script/DeployLeveragedEthTestnet.s.sol
+++ script/DeployLeveragedEthTestnet.s.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
-import "forge-std/console2.sol";
-import "forge-std/Test.sol";
-
 import {DeployLeveragedEth} from "./base/DeployLeveragedEth.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {MockWETH} from "../test/mocks/MockWETH.sol";
 import {MockUSDC} from "../test/mocks/MockUSDC.sol";
+import {GoerliConstants as C} from "../src/lib/GoerliConstants.sol";
 
 contract DeployScript is DeployLeveragedEth {
     function deployMockTokens() internal override {
-        weth = MockWETH(payable(0xA4584d62299915a288bfe223D298db41cB0c9f7f));
-        usdc = MockUSDC(0x30e2B7a907997fDC5a71E377Ece54FAae9F5392D);
+        weth = MockWETH(payable(C.WETH));
+        usdc = MockUSDC(C.USDC);
     }
 
     function run() external {
