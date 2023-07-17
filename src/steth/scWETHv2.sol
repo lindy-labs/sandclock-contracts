450ad16e6daf114458afeeac45553d14f74d896f
realdiganta <47485188+dxganta@users.noreply.github.com>
2023-07-17 19:04:50 +0530

    spelling fix

--- src/steth/scWETHv2.sol
+++ src/steth/scWETHv2.sol
@@ -103,7 +103,7 @@ contract scWETHv2 is BaseV2Vault {
     }
 
     /// @notice swap weth to wstEth
-    /// @dev the keeper will mostly used 0x (zeroExSwap method) for swapping weth to wstEth between rebalancing
+    /// @dev the keeper will mostly use 0x (zeroExSwap method) for swapping weth to wstEth between rebalancing
     /// @dev this method is just a precaution and to be only used by the keeper in case zeroEx API goes down
     /// @param _wethAmount amount of weth to be swapped to wstEth
     function swapWethToWstEth(uint256 _wethAmount) external {
