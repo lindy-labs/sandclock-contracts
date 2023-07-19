b58a5855b0ade687168ec8b29d4c3a2299eeca0e
0xrin1 <0xrin1@protonmail.com>
2023-07-19 16:23:09 +0200

    remove redundant deposits

--- script/DeployLeveragedEthMainnet.s.sol
+++ script/DeployLeveragedEthMainnet.s.sol
@@ -21,13 +21,11 @@ contract DeployLeveragedEthMainnet is DeployLeveragedEth {
         vm.startBroadcast(deployerPrivateKey);
 
         // scWETH
-        weth.deposit{value: 0.01 ether}(); // wrap 0.01 ETH into WETH
         console2.log("eth deposited for weth");
         _deposit(scWeth, 0.01 ether); // 0.01 WETH
         console2.log("weth deposited into wethContract");
 
         // scUSDC
-        weth.deposit{value: 0.01 ether}();
         console2.log("eth deposited for weth to swap for usdc");
         _swapWethForUsdc(0.01 ether);
         console2.log("eth swapped for USDC");
