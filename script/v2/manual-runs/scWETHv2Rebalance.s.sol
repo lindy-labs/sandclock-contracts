09b7d8daeca74e6b345275ebf335af75ca2f39f5
realdiganta <47485188+dxganta@users.noreply.github.com>
2023-08-29 13:05:43 +0530

    saved swapData for tests added

--- script/v2/manual-runs/scWETHv2Rebalance.s.sol
+++ script/v2/manual-runs/scWETHv2Rebalance.s.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.13;
 
 import "forge-std/console2.sol";
 import "forge-std/Test.sol";
+import "forge-std/Script.sol";
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {WETH} from "solmate/tokens/WETH.sol";
 import {AccessControl} from "openzeppelin-contracts/access/AccessControl.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {Surl} from "surl/Surl.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
+import {stdJson} from "forge-std/StdJson.sol";
 
 import {Constants as C} from "../../../src/lib/Constants.sol";
 import {MainnetAddresses as MA} from "../../base/MainnetAddresses.sol";
@@ -25,7 +29,8 @@ import {AaveV2ScUsdcAdapter as scUsdcAaveV2Adapter} from "../../../src/steth/scU
 import {MainnetDeployBase} from "../../base/MainnetDeployBase.sol";
 import {IAdapter} from "../../../src/steth/IAdapter.sol";
 import {scWETHv2Helper} from "../../../test/helpers/scWETHv2Helper.sol";
-import {scWETHv2Utils} from "../utils/scWETHv2Utils.sol";
+import {scWETHv2StrategyParams as Params} from "../../base/scWETHv2StrategyParams.sol";
+import {BaseV2Vault} from "../../../src/steth/BaseV2Vault.sol";
 
 /**
  * invests underlying float in the vault
@@ -36,20 +41,121 @@ import {scWETHv2Utils} from "../utils/scWETHv2Utils.sol";
  * Then run the script using
  * forge script script/v2/manual-runs/scWETHv2Rebalance.s.sol --rpc-url http://127.0.0.1:8545 --ffi
  */
-contract scWETHv2Rebalance is scWETHv2Utils {
+contract scWETHv2Rebalance is Script, scWETHv2Helper {
     using FixedPointMathLib for uint256;
+    using Surl for *;
+    using Strings for *;
+    using stdJson for string;
 
     uint256 keeperPrivateKey = uint256(vm.envOr("KEEPER_PRIVATE_KEY", bytes32(0x00)));
 
+    WETH weth = WETH(payable(C.WETH));
+
+    IAdapter public morphoAdapter = IAdapter(MA.SCWETHV2_MORPHO_ADAPTER);
+    IAdapter public compoundV3Adapter = IAdapter(MA.SCWETHV2_COMPOUND_ADAPTER);
+
+    mapping(IAdapter => uint256) public targetLtv;
+
+    // to be used for testing
+    bytes demoSwapData;
+
+    constructor() scWETHv2Helper(scWETHv2(payable(MA.SCWETHV2)), PriceConverter(MA.PRICE_CONVERTER)) {
+        targetLtv[morphoAdapter] = Params.MORPHO_TARGET_LTV;
+        targetLtv[compoundV3Adapter] = Params.COMPOUNDV3_TARGET_LTV;
+    }
+
     function run() external {
         address keeper = keeperPrivateKey != 0 ? vm.addr(keeperPrivateKey) : MA.KEEPER;
-        console2.log("keeper", keeper);
+
         vm.startBroadcast(keeper);
 
         _invest();
-        // _logs();
+        _logs();
 
         vm.stopBroadcast();
+    }
+
+    /// @dev invest the float lying in the vault to morpho and compoundV3
+    /// @dev also reinvests profits made,i.e increases the ltv
+    /// @dev if there is no undelying float in the contract, run this method with _amount=0 to just reinvest profits
+    function _invest() internal {
+        uint256 float = weth.balanceOf(address(vault));
+        uint256 investAmount = float - vault.minimumFloatAmount();
+
+        console2.log("\nInvesting %s weth", investAmount);
+
+        (bytes[] memory callData,, uint256 totalFlashLoanAmount) = _getInvestParams(investAmount);
+
+        vault.rebalance(investAmount, totalFlashLoanAmount, callData);
+    }
+
+    /// @return : supplyBorrowParams, totalSupplyAmount, totalDebtTaken
+    /// @dev : NOTE: ASSUMING ZERO BALANCER FLASH LOAN FEES
+    function _getInvestParams(uint256 _amount) internal returns (bytes[] memory, uint256, uint256) {
+        uint256 stEthRateTolerance = 0.999e18;
+
+        uint256 morphoAmount = _amount.mulWadDown(Params.MORPHO_ALLOCATION_PERCENT);
+        uint256 compoundAmount = _amount.mulWadDown(Params.COMPOUNDV3_ALLOCATION_PERCENT);
+
+        uint256 morphoFlashLoanAmount = _calcSupplyBorrowFlashLoanAmount(morphoAdapter, morphoAmount);
+        uint256 compoundFlashLoanAmount = _calcSupplyBorrowFlashLoanAmount(compoundV3Adapter, compoundAmount);
+
+        uint256 morphoSupplyAmount =
+            priceConverter.ethToWstEth(morphoAmount + morphoFlashLoanAmount).mulWadDown(stEthRateTolerance);
+        uint256 compoundSupplyAmount =
+            priceConverter.ethToWstEth(compoundAmount + compoundFlashLoanAmount).mulWadDown(stEthRateTolerance);
+
+        uint256 totalFlashLoanAmount = morphoFlashLoanAmount + compoundFlashLoanAmount;
+
+        bytes[] memory callData = new bytes[](3);
+
+        uint256 wethSwapAmount = _amount + totalFlashLoanAmount;
+        bytes memory swapData = demoSwapData.length != 0 ? demoSwapData : getSwapDataWethToWstEth(wethSwapAmount);
+
+        callData[0] =
+            abi.encodeWithSelector(BaseV2Vault.zeroExSwap.selector, C.WETH, C.WSTETH, wethSwapAmount, swapData, 0);
+
+        callData[1] = abi.encodeWithSelector(
+            scWETHv2.supplyAndBorrow.selector, morphoAdapter.id(), morphoSupplyAmount, morphoFlashLoanAmount
+        );
+        callData[2] = abi.encodeWithSelector(
+            scWETHv2.supplyAndBorrow.selector, compoundV3Adapter.id(), compoundSupplyAmount, compoundFlashLoanAmount
+        );
+
+        return (callData, morphoSupplyAmount + compoundSupplyAmount, totalFlashLoanAmount);
+    }
+
+    function _calcSupplyBorrowFlashLoanAmount(IAdapter _adapter, uint256 _amount)
+        internal
+        view
+        returns (uint256 flashLoanAmount)
+    {
+        uint256 debt = vault.getDebt(_adapter.id());
+        uint256 collateral = getCollateralInWeth(_adapter);
+
+        uint256 target = targetLtv[_adapter].mulWadDown(_amount + collateral);
+
+        // calculate the flashloan amount needed
+        flashLoanAmount = (target - debt).divWadDown(C.ONE - targetLtv[_adapter]);
+    }
+
+    function getSwapDataWethToWstEth(uint256 _amount) public returns (bytes memory swapData) {
+        string memory url = string(
+            abi.encodePacked(
+                "https://api.0x.org/swap/v1/quote?buyToken=0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0&sellToken=WETH&sellAmount=",
+                _amount.toString()
+            )
+        );
+
+        string[] memory headers = new string[](1);
+        headers[0] = string(abi.encodePacked("0x-api-key: ", vm.envString("ZEROX_API_KEY")));
+        (uint256 status, bytes memory data) = url.get(headers);
+
+        require(status == 200, "0x GET request Failed");
+
+        string memory json = string(data);
+
+        swapData = json.readBytes(".data");
     }
 
     function _logs() internal view {
@@ -61,5 +167,10 @@ contract scWETHv2Rebalance is scWETHv2Utils {
         console2.log("\n Total Assets %s weth", vault.totalAssets());
         console2.log("\n Net Leverage", getLeverage());
         console2.log("\n Net LTV", debt.divWadUp(collateralInWeth));
+    }
+
+    ///////////////////////////////// FOR TESTING ONLY ///////////////////////////
+    function setDemoSwapData(bytes memory _data) external {
+        demoSwapData = _data;
     }
 }
