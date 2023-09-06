c6ce044edca2f03dfce350398716d97c22c2f5c5
brkomir <nenad.milenkovic154@outlook.com>
2023-09-06 12:24:49 +0200

    reduce script dependecy on .env file

--- script/base/ScUsdcV2ScriptBase.sol
+++ script/base/ScUsdcV2ScriptBase.sol
@@ -20,11 +20,12 @@ import {AaveV3ScUsdcAdapter} from "../../src/steth/scUsdcV2-adapters/AaveV3ScUsd
  */
 abstract contract ScUsdcV2ScriptBase is Script {
     uint256 keeperPrivateKey = uint256(vm.envOr("KEEPER_PRIVATE_KEY", bytes32(0x0)));
+    // if keeper private key is not provided, use the default keeper address for running the script tests
     address keeper = keeperPrivateKey != 0 ? vm.addr(keeperPrivateKey) : MainnetAddresses.KEEPER;
 
     scUSDCv2 public scUsdcV2 = scUSDCv2(vm.envOr("SC_USDC_V2", MainnetAddresses.SCUSDCV2));
 
-    PriceConverter priceConverter = PriceConverter(MainnetAddresses.PRICE_CONVERTER);
+    PriceConverter priceConverter = PriceConverter(vm.envOr("PRICE_CONVERTER", MainnetAddresses.PRICE_CONVERTER));
 
     MorphoAaveV3ScUsdcAdapter public morphoAdapter = MorphoAaveV3ScUsdcAdapter(MainnetAddresses.SCUSDCV2_MORPHO_ADAPTER);
     AaveV2ScUsdcAdapter public aaveV2Adapter = AaveV2ScUsdcAdapter(MainnetAddresses.SCUSDCV2_AAVEV2_ADAPTER);
