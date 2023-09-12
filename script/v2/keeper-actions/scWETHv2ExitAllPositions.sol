3813f8dc3e6e8d5bc05799a4a5d302aa02044a98
realdiganta <47485188+dxganta@users.noreply.github.com>
2023-09-12 18:15:13 +0530

    init

---
+++ script/v2/keeper-actions/scWETHv2ExitAllPositions.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "forge-std/Test.sol";
+import "forge-std/Script.sol";
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {WETH} from "solmate/tokens/WETH.sol";
+import {AccessControl} from "openzeppelin-contracts/access/AccessControl.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {Surl} from "surl/Surl.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+
+import {Constants as C} from "../../../src/lib/Constants.sol";
+import {MainnetAddresses as MA} from "../../base/MainnetAddresses.sol";
+import {ISwapRouter} from "../../../src/interfaces/uniswap/ISwapRouter.sol";
+import {sc4626} from "../../../src/sc4626.sol";
+import {scWETHv2} from "../../../src/steth/scWETHv2.sol";
+import {scUSDCv2} from "../../../src/steth/scUSDCv2.sol";
+import {Swapper} from "../../../src/steth/Swapper.sol";
+import {PriceConverter} from "../../../src/steth/PriceConverter.sol";
+import {AaveV3ScWethAdapter as scWethAaveV3Adapter} from "../../../src/steth/scWethV2-adapters/AaveV3ScWethAdapter.sol";
+import {CompoundV3ScWethAdapter as scWethCompoundV3Adapter} from
+    "../../../src/steth/scWethV2-adapters/CompoundV3ScWethAdapter.sol";
+import {AaveV3ScUsdcAdapter as scUsdcAaveV3Adapter} from "../../../src/steth/scUsdcV2-adapters/AaveV3ScUsdcAdapter.sol";
+import {AaveV2ScUsdcAdapter as scUsdcAaveV2Adapter} from "../../../src/steth/scUsdcV2-adapters/AaveV2ScUsdcAdapter.sol";
+import {MainnetDeployBase} from "../../base/MainnetDeployBase.sol";
+import {IAdapter} from "../../../src/steth/IAdapter.sol";
+import {scWETHv2Helper} from "../../../test/helpers/scWETHv2Helper.sol";
+import {BaseV2Vault} from "../../../src/steth/BaseV2Vault.sol";
+
+/**
+ * @notice Exit all positions and withdraw all funds as WETH to the vault
+ */
+contract scWETHv2ExitAllPositions is Script {
+    using FixedPointMathLib for uint256;
+
+    uint256 keeperPrivateKey = uint256(vm.envOr("KEEPER_PRIVATE_KEY", bytes32(0x00)));
+
+    function run() external {
+        address keeper = keeperPrivateKey != 0 ? vm.addr(keeperPrivateKey) : MA.KEEPER;
+
+        _logs("-----------Before Exit----------------\n");
+
+        vm.startBroadcast(keeper);
+
+        _withdrawAll();
+
+        _logs("------------After Exit------------------\n");
+
+        vm.stopBroadcast();
+    }
+
+    function _logs(string memory msg) internal {
+        console2.log(msg);
+
+        uint256 collateralInWeth = priceConverter.wstEthToEth(vault.totalCollateral());
+        uint256 debt = vault.totalDebt();
+
+        console2.log("\n Total Collateral %s weth", collateralInWeth);
+        console2.log("\n Total Debt %s weth", debt);
+        console2.log("\n Invested Amount %s weth", collateralInWeth - debt);
+        console2.log("\n Total Assets %s weth", vault.totalAssets());
+        console2.log("\n Float %s weth", weth.balanceOf(address(vault)));
+    }
+}
