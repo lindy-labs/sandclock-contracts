eda950c2a42b43c6333537e86c145aa54251cbe8
brkomir <nenad.milenkovic154@outlook.com>
2023-09-20 14:33:32 +0200

    remove "isUsed" script param & code cleanup

--- test/script/RebalanceScUsdcV2.s.t.sol
+++ test/script/RebalanceScUsdcV2.s.t.sol
@@ -333,7 +333,6 @@ contract RebalanceScUsdcV2Test is Test {
         script.setAaveV3InvestableAmountPercent(1e18); // 100%
         uint256 newAaveV3TargetLtv = 0.5e18;
         script.setAaveV3TargetLtv(newAaveV3TargetLtv); // 50%
-        script.setUseAaveV3(true);
 
         uint256 expectedFloat = vault.totalAssets().mulWadDown(vault.floatPercentage());
         uint256 investableAmount = vault.usdcBalance() - expectedFloat;
@@ -343,6 +342,23 @@ contract RebalanceScUsdcV2Test is Test {
 
         assertApproxEqRel(vault.getCollateral(aaveV3.id()), investableAmount, 0.001e18, "aave v3 collateral");
         assertApproxEqRel(vault.getDebt(aaveV3.id()), expectedAaveV3Debt, 0.001e18, "aave v3 debt");
+    }
+
+    function test_run_failsIfInvestingWithUnsupportedAdapter() public {
+        deal(address(vault.asset()), address(this), 100e6);
+        vault.asset().approve(address(vault), 100e6);
+        vault.deposit(100e6, address(this));
+
+        assertTrue(!vault.isSupported(aaveV3.id()), "aave v3 shouldn't be supported");
+
+        script.setMorphoInvestableAmountPercent(0);
+        script.setAaveV2InvestableAmountPercent(0);
+        script.setAaveV3InvestableAmountPercent(1e18); // 100%
+        uint256 newAaveV3TargetLtv = 0.5e18;
+        script.setAaveV3TargetLtv(newAaveV3TargetLtv); // 50%
+
+        vm.expectRevert(abi.encodePacked(RebalanceScUsdcV2.ScriptCannotUseUnsupportedAdapter.selector, aaveV3.id()));
+        script.run();
     }
 
     function test_run_sellsProfitsAndReinvests() public {
@@ -474,7 +490,6 @@ contract RebalanceScUsdcV2Test is Test {
             vm.prank(Constants.MULTISIG);
             vault.addAdapter(aaveV3);
             assertTrue(vault.isSupported(aaveV3.id()), "aave v3 not supported");
-            script.setUseAaveV3(true);
         }
     }
 
@@ -495,10 +510,6 @@ contract RebalanceScUsdcV2TestHarness is RebalanceScUsdcV2 {
         maxProfitSellSlippage = _maxProfitSellSlippage;
     }
 
-    function setUseMorpho(bool _isUsed) public {
-        useMorpho = _isUsed;
-    }
-
     function setMorphoTargetLtv(uint256 _newTargetLtv) public {
         morphoTargetLtv = _newTargetLtv;
     }
@@ -507,20 +518,12 @@ contract RebalanceScUsdcV2TestHarness is RebalanceScUsdcV2 {
         morphoInvestableAmountPercent = _newInvestableAmountPercent;
     }
 
-    function setUseAaveV2(bool _isUsed) public {
-        useAaveV2 = _isUsed;
-    }
-
     function setAaveV2TargetLtv(uint256 _newTargetLtv) public {
         aaveV2TargetLtv = _newTargetLtv;
     }
 
     function setAaveV2InvestableAmountPercent(uint256 _newInvestableAmountPercent) public {
         aaveV2InvestableAmountPercent = _newInvestableAmountPercent;
-    }
-
-    function setUseAaveV3(bool _isUsed) public {
-        useAaveV3 = _isUsed;
     }
 
     function setAaveV3TargetLtv(uint256 _newTargetLtv) public {
