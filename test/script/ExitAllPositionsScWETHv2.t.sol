ed6d8aa5ab7f8e8403476da6be6f969241cf9c75
Diganta Kalita <digantakalita.ai@gmail.com>
2023-09-21 19:47:08 +0530

    Script to Exit All Positions for scWETHv2 (#128)
    * env.example mistake
    
    * scWETHv2Utils
    
    * failing at rebalance
    
    * initial invest working
    
    * unecessary test code removed
    
    * merge conflict fixed
    
    * conflicts
    
    * forge fmt
    
    * formatting
    
    * wip using 0xswap for weth to wstEth swap
    
    * zeroEx api request sending logic pass
    
    * get http request working
    
    * stack too deep fixed
    
    * wip
    
    * still failing
    
    * wip
    
    * scWETHv2 Rebalance tests added on respective script test file
    
    * keeper private key
    
    * saved swapData for tests added
    
    * params moved to main script
    
    * updateable buttons to script
    
    * cleaning
    
    * todos
    
    * disinvest code added to script. compiling
    
    * test harness added
    
    * disinvest test wip
    
    * 0x error handling
    
    * invest & disinvest tests added
    
    * calcRebalanceFlashLoanAmount
    
    * wip
    
    * rebalance script code compiling
    
    * rebalance new script tests passing
    
    * comments removed and totalInvested check added
    
    * moved calldata to storage
    
    * more tests
    
    * moved stEthRateTolearnce to storage
    
    * adapter support check
    
    * logs update and folder name change
    
    * Revert "logs update and folder name change"
    
    This reverts commit 8c19504315d9eb737c52514e4ba38dfb3a7b2e9f.
    
    * Revert "logs update and folder name change"
    
    This reverts commit 8c19504315d9eb737c52514e4ba38dfb3a7b2e9f.
    
    * _logs bug fix
    
    * init
    
    * tests added
    
    * comments
    
    * finish
    
    * docs
    
    * remove unused imports
    
    * assert collateral and debt positions before exit
    
    * merge conflicts fixed

---
+++ test/script/ExitAllPositionsScWETHv2.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {IPool} from "aave-v3/interfaces/IPool.sol";
+import {IAToken} from "aave-v3/interfaces/IAToken.sol";
+import {IVariableDebtToken} from "aave-v3/interfaces/IVariableDebtToken.sol";
+import {Errors} from "aave-v3/protocol/libraries/helpers/Errors.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Address} from "openzeppelin-contracts/utils/Address.sol";
+import {WETH} from "solmate/tokens/WETH.sol";
+
+import {Constants as C} from "../../src/lib/Constants.sol";
+import {RebalanceScWethV2} from "../../script/v2/keeper-actions/RebalanceScWethV2.s.sol";
+import {ExitAllPositionsScWethV2} from "../../script/v2/keeper-actions/ExitAllPositionsScWethV2.s.sol";
+
+import {scWETHv2} from "../../src/steth/scWETHv2.sol";
+import {IAdapter} from "../../src/steth/IAdapter.sol";
+
+contract ExitAllPositionsScWETHv2Test is Test {
+    using FixedPointMathLib for uint256;
+    using Address for address;
+
+    uint256 mainnetFork;
+
+    RebalanceScWethV2TestHarness rebalanceScript;
+    ExitAllPositionsScWethV2 exitScript;
+    scWETHv2 vault;
+    WETH weth = WETH(payable(C.WETH));
+
+    function setUp() public {
+        vm.createFork(vm.envString("RPC_URL_MAINNET"));
+        vm.selectFork(mainnetFork);
+        vm.rollFork(18018649);
+        rebalanceScript = new RebalanceScWethV2TestHarness();
+        exitScript = new ExitAllPositionsScWethV2();
+        vault = exitScript.vault();
+    }
+
+    function testScriptExitsAllPositions(uint256 amount) public {
+        amount = bound(amount, 15 ether, 100 ether);
+        vault.deposit{value: amount}(address(this));
+
+        // run rebalance script to invest
+        rebalanceScript.run();
+
+        uint256 float = weth.balanceOf(address(vault));
+
+        assertApproxEqRel(
+            vault.totalAssets() - float, amount - vault.minimumFloatAmount(), 0.005e18, "Investment failure"
+        );
+
+        assertGe(vault.totalCollateral(), amount, "totalCollateral error");
+        assertGe(vault.totalDebt(), amount, "totalDebt error");
+
+        // exit all positions
+        exitScript.run();
+
+        assertApproxEqRel(weth.balanceOf(address(vault)), amount, 0.005e18, "exit Positions fail");
+        assertLt(vault.totalCollateral(), 10, "collateral not zero");
+        assertLt(vault.totalDebt(), 10, "debt not zero");
+    }
+}
+
+contract RebalanceScWethV2TestHarness is RebalanceScWethV2 {
+    bytes testSwapData;
+
+    function getSwapData(uint256, address, address) public view override returns (bytes memory swapData) {
+        return testSwapData;
+    }
+}
