0b043362ccb1240235a9b495c1486f6d93cd63d3
brkomir <nenad.milenkovic154@outlook.com>
2024-11-11 13:12:12 +0100

    minor update

--- script/v2/keeper-actions/ExitAllPositionsScUsdcV2.s.sol
+++ script/v2/keeper-actions/ExitAllPositionsScUsdcV2.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import {scCrossAssetYieldVault} from "src/steth/scCrossAssetYieldVault.sol";
 import {scCrossAssetYieldVaultExitAllPositionsScript} from
